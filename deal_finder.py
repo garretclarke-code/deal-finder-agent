c2975c5e73e893664cbb005fe233854cc1e14756
garretclarke-code <garret.clarke@gmail.com>
2026-03-05 17:20:56 -0500

    Update deal_finder.py

--- deal_finder.py
+++ deal_finder.py
@@ -14,17 +14,18 @@ soup = BeautifulSoup(response.text, "html.parser")
 titles = []
 links = []
 
-for listing in soup.find_all("a"):
-    if "listing" in str(listing.get("class")):
+for listing in soup.find_all("a", href=True):
+    if "/business-for-sale/" in listing["href"]:
         titles.append(listing.text.strip())
-        links.append(listing.get("href"))
+        links.append("https://www.bizbuysell.com" + listing["href"])
 
 data = pd.DataFrame({
     "Business": titles,
     "Link": links
 })
 
+data = data.drop_duplicates()
+
 data.to_csv("business_leads.csv", index=False)
 
 print("Done. Leads saved to business_leads.csv")
-
